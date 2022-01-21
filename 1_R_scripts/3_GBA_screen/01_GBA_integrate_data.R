087bc82e7fb65cb4bea5a333f9994ae292d1ffec
Lukas_1 <lukas.frick@gmail.com>
2022-01-21 11:46:06 +0100

    Moved code for excluding problematic control wells to its correct place in the "integrate GBA data" script

--- 1_R_scripts/3_GBA_screen/01_GBA_integrate_data.R
+++ 1_R_scripts/3_GBA_screen/01_GBA_integrate_data.R
@@ -8,7 +8,7 @@ input_dir         <- file.path(project_dir, "2_input")
 rdata_dir         <- file.path(project_dir, "3_R_objects")
 general_rdata_dir <- file.path(rdata_dir, "1_General")
 GBA_rdata_dir     <- file.path(rdata_dir, "2_GBA")
-raw_dir           <- file.path(input_dir, "GBA_data")
+raw_data_dir      <- file.path(input_dir, "GBA_data")
 
 
 
@@ -35,13 +35,13 @@ load(file.path(general_rdata_dir, "01_convert_plate_layouts.RData"))
 
 # Read in data ------------------------------------------------------------
 
-raw_files_vec <- list.files(raw_dir)
+raw_files_vec <- list.files(raw_data_dir)
 are_luminescence <- grepl("Luminescence", raw_files_vec, fixed = TRUE)
 GBA_files_vec <- raw_files_vec[!are_luminescence]
 GBA_files_vec <- SortByRoman(GBA_files_vec)
 
 GBA_df_list <- sapply(GBA_files_vec, function(x) {
-  read.csv(file.path(raw_dir, x), header = FALSE)
+  read.csv(file.path(raw_data_dir, x), header = FALSE)
 }, simplify = FALSE)
 
 GBA_signal_vec_list <- lapply(GBA_df_list, function(x) {
@@ -51,7 +51,7 @@ GBA_signal_vec_list <- lapply(GBA_df_list, function(x) {
 luminescence_files_vec <- raw_files_vec[are_luminescence]
 luminescence_files_vec <- SortByRoman(luminescence_files_vec)
 lum_df_list <- sapply(luminescence_files_vec, function(x) {
-  read.csv(file.path(raw_dir, x), skip = 9)[1:16, 2:25]
+  read.csv(file.path(raw_data_dir, x), skip = 9)[1:16, 2:25]
 }, simplify = FALSE)
 
 lum_signal_vec_list <- lapply(lum_df_list, function(x) {
@@ -59,7 +59,19 @@ lum_signal_vec_list <- lapply(lum_df_list, function(x) {
 })
 
 
-# Integrate signal values into layout_df ----------------------------------
+
+# Exclude problematic control wells from 'layout_df' ----------------------
+## These were known a priori (dispensing issues!)
+
+columns_1to4 <- matrix(seq_len(384), nrow = 16, ncol = 24, byrow = TRUE)[, 1:4]
+are_problematic <- (layout_df[, "Plate_number_384"] == "X") &
+                   (layout_df[, "Well_number_384"] %in% columns_1to4)
+layout_df[are_problematic, "Is_NT_ctrl"] <- FALSE
+layout_df[are_problematic, "Is_pos_ctrl"] <- FALSE
+
+
+
+# Integrate measurement data with 'layout_df' -----------------------------
 
 dup1_indices <- seq(from = 1, to = 23, by = 2)
 dup2_indices <- seq(from = 2, to = 24, by = 2)
@@ -74,6 +86,7 @@ GBA_df <- data.frame(
   "Raw_rep1" = GBA_dup1_vec,
   "Raw_rep2" = GBA_dup2_vec
 )
+
 
 
 # Save data ---------------------------------------------------------------
